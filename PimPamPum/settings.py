18abf3fecc00695f90c019def0115f9da0603ace
AbielBorja <abielboga@gmail.com>
2022-05-02 21:54:21 -0500

    Final

--- PimPamPum/settings.py
+++ PimPamPum/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'pages',
+    'treblle',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'treblle.middleware.TreblleMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -124,3 +126,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+TREBLLE_INFO = {
+    'api_key': os.environ.get('TREBLLE_API_KEY'),
+    'project_id': os.environ.get('TREBLLE_PROJECT_ID')
+}
